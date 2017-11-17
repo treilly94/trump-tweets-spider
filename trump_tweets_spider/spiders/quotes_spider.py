c59d7bb5166d8f9285fcf50f9ae5420048ebfcd9
treilly94 <treilly94@live.com>
2017-11-17 15:56:31 +0000

    added added more detail to the div tags to prevent issues with multiple ids

--- trump_tweets_spider/spiders/quotes_spider.py
+++ trump_tweets_spider/spiders/quotes_spider.py
@@ -10,8 +10,9 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for tweet in response.css("div.tweet"):
             yield {
-                'tweet_id': tweet.css('div::attr(data-tweet-id)').extract(),
-                'user_id': tweet.css('div::attr(data-user-id)').extract(),
-                'permalink': tweet.css('div::attr(data-permalink-path)').extract(),
+                'tweet_id': tweet.css('div.tweet::attr(data-tweet-id)').extract(),
+                'user_id': tweet.css('div.tweet::attr(data-user-id)').extract(),
+                'timestamp': tweet.css('span._timestamp::attr(data-time-ms)').extract(),
+                'permalink': tweet.css('div.tweet::attr(data-permalink-path)').extract(),
                 'tweet_text': tweet.css('p.tweet-text::text').extract(),
             }
