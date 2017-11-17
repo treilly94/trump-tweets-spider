1d230d58e2214da043ce553438eef0a29d2be7c4
treilly94 <treilly94@live.com>
2017-11-17 15:42:08 +0000

    added items to extract

--- trump_tweets_spider/spiders/quotes_spider.py
+++ trump_tweets_spider/spiders/quotes_spider.py
@@ -10,5 +10,8 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for tweet in response.css("div.tweet"):
             yield {
-                'text': tweet.css('p.tweet-text::text').extract(),
+                'tweet_id': tweet.css('div::attr(data-tweet-id)').extract(),
+                'user_id': tweet.css('div::attr(data-user-id)').extract(),
+                'permalink': tweet.css('div::attr(data-permalink-path)').extract(),
+                'tweet_text': tweet.css('p.tweet-text::text').extract(),
             }
